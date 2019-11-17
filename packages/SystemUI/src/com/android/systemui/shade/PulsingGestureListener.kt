9c227620c8fd2bbc9c134dfd59e0a15037fb1328
maxwen <max.weninger@gmail.com>
2019-11-18 00:29:15 +0100

    fixup! SystemUI: use DOUBLE_TAP_TO_WAKE setting also for wake from aod
    Co-authored-by: Michael W <baddaemon87@gmail.com>
    Change-Id: Ib51fdeaaa9a1b18b79f4f311c65565352d909a72

--- packages/SystemUI/src/com/android/systemui/shade/PulsingGestureListener.kt
+++ packages/SystemUI/src/com/android/systemui/shade/PulsingGestureListener.kt
@@ -60,17 +60,16 @@ class PulsingGestureListener @Inject constructor(
     private var doubleTapEnabledNative = false
 
     init {
-        val tunable = Tunable { key: String?, _: String? ->
+        val tunable = Tunable { key: String?, value: String? ->
             when (key) {
+                Settings.Secure.DOUBLE_TAP_TO_WAKE ->
+                    doubleTapEnabledNative = TunerService.parseIntegerSwitch(value, false)
                 Settings.Secure.DOZE_DOUBLE_TAP_GESTURE ->
                     doubleTapEnabled = ambientDisplayConfiguration.doubleTapGestureEnabled(
                             UserHandle.USER_CURRENT)
                 Settings.Secure.DOZE_TAP_SCREEN_GESTURE ->
                     singleTapEnabled = ambientDisplayConfiguration.tapGestureEnabled(
                             UserHandle.USER_CURRENT)
-                Settings.Secure.DOUBLE_TAP_TO_WAKE ->
-                    doubleTapEnabledNative = Settings.Secure.getIntForUser(notificationShadeWindowView.getContext().getContentResolver(),
-                            Settings.Secure.DOUBLE_TAP_TO_WAKE, 0, UserHandle.USER_CURRENT) == 1
             }
         }
         tunerService.addTunable(tunable,
@@ -122,6 +121,7 @@ class PulsingGestureListener @Inject constructor(
     override fun dump(pw: PrintWriter, args: Array<out String>) {
         pw.println("singleTapEnabled=$singleTapEnabled")
         pw.println("doubleTapEnabled=$doubleTapEnabled")
+        pw.println("doubleTapEnabledNative=$doubleTapEnabledNative")
         pw.println("isDocked=${dockManager.isDocked}")
         pw.println("isProxCovered=${falsingManager.isProximityNear}")
     }
