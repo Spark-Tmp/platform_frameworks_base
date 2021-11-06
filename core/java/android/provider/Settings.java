dea57a1cc9bd1c9e656f3415c950f50e2ddc74e1
SagarMakhar <sagarmakhar@gmail.com>
2021-11-06 13:45:39 +0000

    Make Udfps haptic feedback optional [1/2]
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6222,6 +6222,12 @@ public final class Settings {
         public static final String QQS_LAYOUT = "qqs_layout";
 
         /**
+         * Whether to play haptic feedback when touching udfps icon
+         * @hide
+         */
+        public static final String UDFPS_HAPTIC_FEEDBACK = "udfps_haptic_feedback";
+
+        /**
          * These are all public system settings
          *
          * @hide
